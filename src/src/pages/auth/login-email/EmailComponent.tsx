aff3b6180c527fe603ccbf9bf90b0af1e95c2679
Crustbrns <crustbrns@gmail.com>
2025-02-16 17:00:12 +0100

    Added error message & values

--- src/src/pages/auth/login-email/EmailComponent.tsx
+++ src/src/pages/auth/login-email/EmailComponent.tsx
@@ -1,10 +1,24 @@
 import { Button, Checkbox, Input, Space, Typography } from "antd";
 import AllgemeinComponent from "../allgemein-component/AllgemeinComponent";
 import React from "react";
+import validator from "validator";
+import ErrorMessage from "../../../components/error/ErrorMessage";
 
 function EmailComponent() {
   const [email, setEmail] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError("");
+    setEmail(e.target.value);
+  };
+
+  const handleLogin = () => {
+    if (validator.isEmail(email)) {
+      setLoading(true);
+    } else setError("Ungültige E-Mail-Adresse");
+  };
 
   return (
     <AllgemeinComponent>
@@ -18,12 +32,15 @@ function EmailComponent() {
           </Typography.Text>
           <Input
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => handleEmailChange(e)}
             placeholder="Deine E-Mail"
           />
+          <ErrorMessage text={error}/>
         </div>
         <Button
+          disabled={loading}
           loading={loading}
+          onClick={handleLogin}
           className="w-full mt-4"
           style={{ backgroundColor: "#15cdb9", height: 40 }}
         >
